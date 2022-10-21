db2bdeb3046dddf6e6b6c6ee1f4a35cbe17bbd34
Mikhail Kropa <kropa@remondis.ru>
2022-10-21 18:36:33 +0300

    Решил задачу 2 из ДЗ

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+ Console.WriteLine ($"{max} - максимальное число");
+
