3f6ce6505a44a8ce59a7979a7663e94130923a14
Mikhail Kropa <kropa@remondis.ru>
2022-10-21 18:10:11 +0300

    Решили задачу 1 из ДЗ

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+ Console.WriteLine ($"{num1} - большее число, а {num2} - меньшее число");
+else if (num2 > num1)
+ Console.WriteLine ($"{num2} - большее число, а {num1} - меньшее число");
+else if (num1 == num2)
+ Console.WriteLine ("Числа равны");
+else
+ Console.WriteLine ("Числа не равны");   
+
+
