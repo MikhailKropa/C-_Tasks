5a3861a6564c4471bc18db423047ebf50ed4a3e7
Mikhail Kropa <kropa@remondis.ru>
2022-10-24 16:00:07 +0300

    Решил задачу 13 из семинара 2

---
+++ Task_6_Seminar_2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите  число: ");
+string num = Convert.ToString(Console.ReadLine());
+
+    if(num.Length >= 3)
+      Console.WriteLine(num[2]);
+      
+        else    
+           Console.WriteLine("третьей цифры нет");
